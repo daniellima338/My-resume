7d5961bb420ad303954979ccf44285d91a033ad9
daniellima338 <daniellima338@gmail.com>
2021-06-28 11:40:23 +0000

    fixed bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -67,7 +67,7 @@ function fetchGitHubInformation(event) {
                     `<h2>No info found for user ${username}</h2>`);
             } else if(errorResponse.status === 403) {
                 var resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset")*1000);
-                $("gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
